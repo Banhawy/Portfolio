0d676c91964501f02b25f003f07dd2dff032f0b9
Banhawy <the.benhawy@gmail.com>
2017-01-07 23:34:52 -0600

    added Masonry Layout to Projects Section

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,17 @@ $(document).ready(function() {
   $("#welp").addClass("animated slideInLeft");
   $("#target1").addClass("fadeInRight");
   $('body').scrollspy({ target: '#spy'});
+
+ var $grid = $('.grid').masonry({
+  // options...
+	  itemSelector: '.grid-item',
+	  columnWidth: '.grid-sizer',
+	  perventPosition: true,
+	  gutter: 10
+	});
+ $grid.imagesLoaded().progress( function() {
+	  $grid.masonry();
+	});
+
   });
 
